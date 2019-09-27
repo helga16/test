10a0ad6a041b59421056faf4939e7373d07c760e
Helga <webdevassistant@gmail.com>
2019-09-27 12:24:09 +0200

    Func add var

--- 3.js
+++ 3.js
@@ -1,6 +1,7 @@
 function authenticate (login, password) {
   if (login === 'login' && password === 'password') {
-    return 'You were logged in';
+    var bsr = 'hghgghg';
+	return 'You were logged in';
   } else {
     return 'Login is incorrect';
   }
